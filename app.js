b838c431fd5d2258f6630adf34a2240f435a6bea
Dipesh-Budhiraja <25dipesh98@gmail.com>
2017-10-08 19:41:14 +0530

    passport config

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var handleBars = require('express-handlebars');
 var mongoose = require('mongoose');
-var index = require('./routes/index');
-var users = require('./routes/users');
 const session = require('express-session');
 const passport = require('passport');
 var flash=require('connect-flash');
 const validator = require('express-validator');
-mongoose.connect('localhost:27017/practice');
 
+var index = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
+mongoose.connect('localhost:27017/practice');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-// app.use('/users', users);
+app.use('/user', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
