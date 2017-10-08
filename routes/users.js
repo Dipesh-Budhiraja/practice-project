b43cb6d7e5990760196a87e9e01c57a19c2e07b6
Dipesh-Budhiraja <25dipesh98@gmail.com>
2017-10-08 18:39:35 +0530

    user routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf');
+var passport = require('passport');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
+
+router.get('/profile', isLoggedIn, function(req, res, next){
+    res.render('user/profile');
+});
+
+router.get('/logout', isLoggedIn, function(req, res, next){
+    req.logout();
+    res.redirect('/');
+});
+
+router.use('/', notLoggedIn, function(req, res, next){
+    next();
+});
+
+router.get('/signup', function(req, res, next){
+    res.render('user/signup', {csrfToken: res.csrfToken()});
+});
+
+router.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signup',
+    failureFlash: true
+}));
+
+router.get('/signin', function(req, res, next){
+    res.render('/user/signin', {csrfToken: res.csrfToken()});
+});
+
+router.post('/signin', passport.authenticate('local-signin', {
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signin',
+    failureFlash: true
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,3 +45,17 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/');
+}
+
+function notLoggedIn(req, res, next){
+    if(!req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/');
+}
